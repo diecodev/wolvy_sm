1185f55420bdc00ab6e778fe85b6340781ef8e7b
diecodev <diegoddiaz05@gmail.com>
2022-12-11 23:29:46 -0500

    Fix: bg effect when mobile scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react'
 import { Inter } from '@next/font/google'
 import '@styles/globals.css'
-import { Logo } from 'app/components/icons'
 
 const inter = Inter({
   display: 'swap',
@@ -20,12 +19,11 @@ const RootLayout: FC<IRootLayout> = ({ children }) => {
     <html lang='es' className={inter.className} style={inter.style}>
       <head />
       <body>
-        <div className='bg_logo'>
-          <Logo />
+        <div className='radial_gradient radial_gradient--one' />
+        <div className='radial_gradient radial_gradient--two' />
+        <div className='home_container'>
+          {children}
         </div>
-        <div className='bg_rounded bg_rounded--grey200' />
-        <div className='bg_rounded bg_rounded--red150' />
-        {children}
       </body>
     </html>
   )

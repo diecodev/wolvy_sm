780ffef12023f211ef30d6205705cefed746de76
diecodev <diegoddiaz05@gmail.com>
2022-12-13 18:05:03 -0500

    Add: redirects config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,19 @@ const nextConfig = {
   swcMinify: true,
   experimental: {
     appDir: true
+  },
+  redirects: async () => {
+    return [
+      {
+        source: '/instagram',
+        destination: 'https://instagram.com/wolvy_sm',
+        permanent: true
+      },
+      {
+        source: '/whatsapp',
+        destination: 'https://api.whatsapp.com/send/?phone=573102629919&text&type=phone_number&app_absent=0'
+      }
+    ]
   }
 }
 
